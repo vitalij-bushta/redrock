dd3fee40b57888ec323bb6cc7db8094b649e9f83
Vitalii <vitalij.bushta@gmail.com>
2022-09-17 18:42:01 +0300

    v2.0

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// Модальное окно
+function bindModal(trigger, modal, close) {
+  (trigger = document.querySelector(trigger)),
+    (modal = document.querySelector(modal)),
+    (close = document.querySelector(close));
+
+  const body = document.body;
+
+  trigger.addEventListener("click", (e) => {
+    e.preventDefault();
+    modal.style.display = "flex";
+    body.classList.add("locked");
+  });
+  close.addEventListener("click", () => {
+    modal.style.display = "none";
+    body.classList.remove("locked");
+  });
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+      body.classList.remove("locked");
+    }
+  });
+}
+
+bindModal(".modal__btn", ".modal", ".close");
